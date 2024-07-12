4b4117d4ef1d11a8583340717a2d2e7cf532a032
Kumar-Vishu7 <vk2691777@gmail.com>
2024-07-12 18:12:51 +0530

    Post Api

--- src/main/java/com/sms/student/controller/StudentController.java
+++ src/main/java/com/sms/student/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.sms.student.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class StudentController {
     @GetMapping
     public ResponseEntity<String> getStudent () {
         return ResponseEntity.ok("No students added yet.");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postStudent () {
+        return ResponseEntity.ok("Give some data.");
     }
 }
